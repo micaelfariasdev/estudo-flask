78dc8c1881f2923a98027af4893d7e8fb4716e76
Micael Farias <micaelfarias.dev@gmail.com>
2025-02-28 10:14:00 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,10 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sua_string_de_conexao_aqui'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI", "fallback_key")
     db.init_app(app)
     return app
-    
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=os.getenv("DATABASE_URI", "fallback_key")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
